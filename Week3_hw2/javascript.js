116acad12329bc50a388b1a40199316f236020b4
Dian Rachmawati <diansoraya@MacBook-Air-Aya.local>
2016-09-26 18:14:33 -0400

    update

--- Week3_hw2/javascript.js
+++ Week3_hw2/javascript.js
@@ -9,23 +9,23 @@ var query = "rainbow"
 //Trending Here ya
 
 
-function setup() {
-  noCanvas();
-  var url = 'http://api.giphy.com/v1/gifs/trending?api_key=dc6zaTOxFJmzC';
-  loadJSON(url, gotData);
-}
+// function setup() {
+//   noCanvas();
+//   var url = 'http://api.giphy.com/v1/gifs/trending?api_key=dc6zaTOxFJmzC';
+//   loadJSON(url, gotData);
+// }
 
-function gotData(giphy) {
-  for (var i = 0; i < giphy.data.length; i++) {
-    createImg(giphy.data[i].images.original.url);
-  }
-}
+// function gotData(giphy) {
+//   for (var i = 0; i < giphy.data.length; i++) {
+//     createImg(giphy.data[i].images.original.url);
+//   }
+// }
 
 //Search Function Here ya
 
 function addtoscreen(x)
 {
-    box.value += x;
+    box.value = x;
 
     if (x=='clear')
     {
